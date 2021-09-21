027815b56a406214a6a646c761ce9fa08a68eb67
Reno Deitermann <reno.d@gmx.de>
2021-09-21 22:00:31 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -4,67 +4,55 @@
 
 #define ONBOARD_LED 13
 
-#define X_STEP_PIN                            54
-#define X_DIR_PIN                             55
-#define X_ENABLE_PIN                          38
+#define X_STEP_PIN                            2
+#define X_DIR_PIN                             3
+#define X_ENABLE_PIN                          4
 
-#define Y_STEP_PIN                            60
-#define Y_DIR_PIN                             61
-#define Y_ENABLE_PIN                          56
+#define Y_STEP_PIN                            5
+#define Y_DIR_PIN                             6
+#define Y_ENABLE_PIN                          7
 
-#define Z_STEP_PIN                            46
-#define Z_DIR_PIN                             48
-#define Z_ENABLE_PIN                          62
-
+#define INCREMENT 50000
+#define SPEED 1600
+#define RATIO 2
 
 AccelStepper xStepper(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
 AccelStepper yStepper(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN);
-AccelStepper zStepper(AccelStepper::DRIVER, Z_STEP_PIN, Z_DIR_PIN);
+
+
 
 void setup() {
   pinMode(ONBOARD_LED,OUTPUT);
 
-   xStepper.setMaxSpeed(5000);
-   xStepper.setAcceleration(5000);
+   xStepper.setMaxSpeed(SPEED);
+   xStepper.setAcceleration(2000);
    xStepper.setEnablePin(X_ENABLE_PIN);
    xStepper.setPinsInverted(false, false, true);
-   xStepper.moveTo(10000);
+   xStepper.moveTo(INCREMENT);
    xStepper.enableOutputs();
   
-   yStepper.setMaxSpeed(5000);
-   yStepper.setAcceleration(5000);
+   yStepper.setMaxSpeed(SPEED/RATIO);
+   yStepper.setAcceleration(2000);
    yStepper.setEnablePin(Y_ENABLE_PIN);
    yStepper.setPinsInverted(false, false, true);
-   yStepper.moveTo(10000);
+   yStepper.moveTo(-INCREMENT/RATIO);
    yStepper.enableOutputs();
-
-   zStepper.setMaxSpeed(5000);
-   zStepper.setAcceleration(5000);
-   zStepper.setEnablePin(Z_ENABLE_PIN);
-   zStepper.setPinsInverted(false, false, true);
-   zStepper.moveTo(10000);
-   zStepper.enableOutputs();
   
 }
 
 void loop() {
   xStepper.run();
   yStepper.run();
-  zStepper.run();
+ 
 
   if ( xStepper.distanceToGo() == 0  )
    {
-     xStepper.moveTo(-xStepper.currentPosition());
+     xStepper.moveTo(xStepper.currentPosition()+INCREMENT);
    }
 
 if ( yStepper.distanceToGo() == 0  )
    {
-     yStepper.moveTo(-yStepper.currentPosition());
-   }
-
-if ( zStepper.distanceToGo() == 0  )
-   {
-     zStepper.moveTo(-zStepper.currentPosition());
+     yStepper.moveTo(yStepper.currentPosition()-INCREMENT/RATIO);
    }
 
 }
